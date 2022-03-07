47dbeb0cba962c6db995deed5aced63613a54452
Harish G Krishna <97905727+BlockchainDeveloper009@users.noreply.github.com>
2022-03-07 17:57:16 -0600

    Add files via upload

---
+++ common/prop.js
@@ -0,0 +1,13 @@
+class prop
+{
+    TWITTER_HANDLE = 'fractalfantasy1';
+    TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
+    
+    LINKEDIN_HANDLE = 'fractalfantasy';
+
+    LINKEDIN_LINK = `https://www.linkedin.com/company/${LINKEDIN_HANDLE}/`;
+
+    constructor (){
+
+    }
+}
