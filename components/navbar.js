4d4ca005e9994ae8f202c9f9cd552c212b58ad4e
Harish G Krishna <97905727+BlockchainDeveloper009@users.noreply.github.com>
2022-03-08 16:52:41 -0600

    Home,AboutUs button added to Nav bar

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,13 @@
-import { Flex, Box } from '@chakra-ui/react'
-
+import { Flex, Box, Button } from '@chakra-ui/react'
+import styles from '../styles/Navbar.module.css'
 
 export default function Navbar() {
 
     return (
         <Flex px={'4'} align={'center'} height={'6vh'} bg={'yellow.200'}>
-            <Box>fractal</Box>
+             <Button className={styles.navButton}>Home</Button>
+             <Button className={styles.navButton}>About Us</Button>
+
         </Flex>
     )
 }
-
