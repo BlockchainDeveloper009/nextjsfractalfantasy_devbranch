b331c70d810809d15f2da7e0943085b05d6bb7ef
alpha@DESKTOP-6F6AEHR <graphmywork@gmail.com>
2022-03-14 23:56:05 -0500

    merge the pages

--- pages/getFractals.js
+++ pages/getFractals.js
@@ -1,14 +1,18 @@
-import { Button } from '@chakra-ui/react'
+import { Flex,Box, Button } from '@chakra-ui/react'
 import React from 'react'
 
 export default function getFractals() {
-    //https://nft.fractal.is/
+    let fractalUrl = 'https://nft.fractal.is/'
   return (
-    <div>getFractals
-        <Flex>
-            <Button>
-                Get getFractals
-            </Button>
+    <div>
+        
+        <Flex px={'4'} align={'center'} > 
+        <Box width={{ base: '90%', md: '60%' }} fontSize={'4rem'}>
+         You need fractals to downlaod this game!!!
+          </Box>
+        </Flex>
+        <Flex px={'4'} align={'center'} height={'6vh'} bg={'MenuText'}>
+            <Button background={'-webkit-linear-gradient(left, #ff8867, #ff52ff)'} onClick={fractalUrl}>Click here to buy fractals</Button>
         </Flex>
     </div>
   )
