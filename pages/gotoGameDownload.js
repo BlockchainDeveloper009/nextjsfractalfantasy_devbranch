e2820d75696b77c165f28a6f089b1e8902793e6c
Steven Arellano <stevenjarellano2@gmail.com>
2022-03-15 14:47:07 -0500

    gotogamedownload

--- pages/gotoGameDownload.js
+++ pages/gotoGameDownload.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { Flex, Box, Button } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
-export default function gotoGameDownload() {
-    
-    const router = useRouter()
+export default function GotoGameDownload() {
+
+  const router = useRouter();
   function redirectToGameDownloadPage() {
     let downloadUrl = 'https://drive.google.com/file/d/1Rp7d2P3r8sl_tE6iM8szT1g57o1issoV/view?usp=sharing'
-    console.log('redirect to '+downloadUrl)
+    console.log('redirect to ' + downloadUrl)
     router.push(downloadUrl)
   }
   return (
     <div>
 
-        <Flex px={'4'} align={'center'} height={'6vh'} bg={'MenuText'}>
-            <Button background={'-webkit-linear-gradient(left, #ff8867, #ff52ff)'} onClick={redirectToGameDownloadPage}>Download your Game</Button>
-        
-        </Flex>
+      <Flex px={'4'} flexDir='column' justify={'center'} align={'center'} height={'80vh'} bg={'MenuText'}>
+        <Button background={'-webkit-linear-gradient(left, #ff8867, #ff52ff)'} onClick={redirectToGameDownloadPage}>Download your Game</Button>
+
+      </Flex>
 
     </div>
   )
